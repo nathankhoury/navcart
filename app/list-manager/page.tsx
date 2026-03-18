77b24497b07ae117fa103a989385c5c1ad673f68
Nathan Khoury <nathankhoury0709@gmail.com>
2026-03-18 11:48:03 -0400

    add dummy landing page and dummy listmanager nav

---
+++ app/list-manager/page.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function ListManager() {
+    return (
+        <div id="lm-page-wrapper" className="flex bg-white text-black">
+            <div>
+                <p>this is a dummy list management page</p>
+            </div>
+        </div>
+    );
+}
