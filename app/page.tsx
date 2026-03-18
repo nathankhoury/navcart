8386bceb90eacf7ef986041aed1d5a3de108d4f7
Nathan Khoury <nathankhoury0709@gmail.com>
2026-03-18 15:01:04 -0400

    add topbar to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
+import TopBar from "./topbar";
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white text-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-white sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
+    <div className="bg-zinc-50 font-sans dark:bg-white text-black">
+      <TopBar/>
+      <main className="bg-white">
+        <div className="flex gap-6 text-center sm:items-start">
+          <p className="font-semibold text-black">
             This is a dummy landing page until we have a real one :)
-          </h1>
+          </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
