3839a835fb957ac7ded3c4e41339e14c67702495
Nathan Khoury <nathankhoury0709@gmail.com>
2026-03-18 11:51:14 -0400

    add comments for listmanager page

--- app/list-manager/page.tsx
+++ app/list-manager/page.tsx
@@ -1,6 +1,13 @@
 import "./styles.css";
 
+{/*
+    page.tsx is the file that defines the content of the page at the route /list-manager,
+    think of it like the main body of the page, and other .tsx files in the same directory
+    are supporting components that you can import and use in page.tsx
+*/}
+
 export default function ListManager() {
+    {/* return one single parent div (or other element, but div wrapper seems to work best) */}
     return (
         <div id="lm-page-wrapper" className="flex bg-white text-black">
             <div>
